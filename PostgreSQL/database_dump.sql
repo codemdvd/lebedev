959bb749b14951ef029077d55750a9bcaffddba2
Prokhorkot <kotprokhor@gmail.com>
2023-05-29 21:33:40 +0300

    added init.db to docker-compose

--- PostgreSQL/database_dump.sql
+++ PostgreSQL/database_dump.sql
@@ -17,10 +17,127 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: admin78; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE admin78;
+
+
+ALTER DATABASE admin78 OWNER TO postgres;
+
+\connect admin78
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: departments; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.departments (
+    dept_no character(4) NOT NULL,
+    dept_name character varying(40)
+);
+
+
+ALTER TABLE public.departments OWNER TO postgres;
+
+--
+-- Name: employees; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.employees (
+    emp_id integer NOT NULL,
+    first_name character varying(30),
+    second_name character varying(30),
+    emp_login character varying(30),
+    emp_pass character varying(30),
+    emp_phone character varying(17),
+    emp_email character varying(40),
+    dept_no character(4)
+);
+
+
+ALTER TABLE public.employees OWNER TO postgres;
+
+--
+-- Data for Name: departments; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- Data for Name: employees; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- Name: departments departments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.departments
+    ADD CONSTRAINT departments_pkey PRIMARY KEY (dept_no);
+
+
+--
+-- Name: employees employees_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employees
+    ADD CONSTRAINT employees_pkey PRIMARY KEY (emp_id);
+
+
+--
+-- Name: employees employees_dept_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employees
+    ADD CONSTRAINT employees_dept_no_fkey FOREIGN KEY (dept_no) REFERENCES public.departments(dept_no);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+
+
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.5
+-- Dumped by pg_dump version 14.5
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
 -- Name: admin78_order; Type: DATABASE; Schema: -; Owner: postgres
 --
 
-CREATE DATABASE admin78_order WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'Russian_Russia.1251';
+CREATE DATABASE admin78_order;
 
 
 ALTER DATABASE admin78_order OWNER TO postgres;
