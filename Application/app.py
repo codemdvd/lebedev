580bc707415ca5b284cdd3914734b6b58faad76b
Vasily Apasov <102738486+codemdvd@users.noreply.github.com>
2023-05-31 21:25:41 +0300

    order page

--- Application/app.py
+++ Application/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, session, redirect, render_template, url_for, request
-
+from database_engine import *
 app = Flask(__name__)
 
 @app.route('/')
 def auth():
      return render_template('auth.html')
 
-
+@app.route('/home')
+def home():
+     orders = orders_session.query(OrderTable).all()
+     for order in orders:
+          print('Address:', order.address)
+          print('Order List', order.order_list)
+     return render_template('wine_orders.html', orders=orders)
 
 if __name__ == '__main__':
     app.run()
